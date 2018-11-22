930ae727eb1893ba018a2c2e3627dfe49df0f631
hanshungithub <hshanshun@163.com>
2018-11-22 15:46:24 +0800

    abstract bean

--- src/main/java/cn/hassan/spring/impl/BaseBean.java
+++ src/main/java/cn/hassan/spring/impl/BaseBean.java
@@ -10,6 +10,10 @@ public class BaseBean {
 
 	private String beanName;
 
+	public String getBeanName() {
+		return beanName;
+	}
+
 	public void setBeanName(String beanName) {
 		this.beanName = beanName;
 	}
